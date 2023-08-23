dbc880f187d6bf702b86354a867fbbafaa5dbc84
6MB11 <45149917+6MB11@users.noreply.github.com>
2023-08-22 22:04:14 -0400

    Smaller project div on overlay, default display property of overlay moved to HTML which I'm guessing is easier for screenreaders and crawlers to parse

---
+++ projects/static/projects/main.js
@@ -0,0 +1,7 @@
+function on() {
+  document.getElementById("background-overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("background-overlay").style.display = "none";
+}
