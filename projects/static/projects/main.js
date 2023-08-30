daca98c9c4e808b9a085a9b6fe4b18b4655239fd
6MB11 <45149917+6MB11@users.noreply.github.com>
2023-08-29 21:18:15 -0400

    Making just the clicked overlay open/close

--- projects/static/projects/main.js
+++ projects/static/projects/main.js
@@ -1,10 +1,21 @@
-function on() {
-  document.getElementsByClassName("background-overlay")[0].style.display =
-    "inline";
+function on(e) {
+  var element = overlayIterator(e);
+  element.style.display = "block";
+  document.getElement.overflow = "none";
 }
 
 function off(e) {
   if (e.target !== e.currentTarget) return;
-  document.getElementsByClassName("background-overlay")[0].style.display =
-    "none";
+  var element = overlayIterator(e);
+  element.style.display = "none";
+  document.html.overflow = "auto";
+}
+
+function overlayIterator(e) {
+  var overlays = document.getElementsByClassName("background-overlay");
+  for (var i = 0; i < overlays.length; i++) {
+    if (overlays[i].id === e.currentTarget.id) {
+      return overlays[i];
+    }
+  }
 }
