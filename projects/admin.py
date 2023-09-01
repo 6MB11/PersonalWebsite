065e88c6568be9e25eb8891c096e179ffa358bbf
6MB11 <45149917+6MB11@users.noreply.github.com>
2023-08-31 22:02:05 -0400

    Tab model

--- projects/admin.py
+++ projects/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Project, ExternalSite
+from .models import Project, Tab, ExternalSite
 
 # Register your models here.
 admin.site.register(Project)
+admin.site.register(Tab)
 admin.site.register(ExternalSite)
