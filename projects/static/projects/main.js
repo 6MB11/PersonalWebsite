5487cf34034154b76334b9c2897f031721dcdc66
6MB11 <45149917+6MB11@users.noreply.github.com>
2023-09-10 21:35:51 -0400

    Show tab text on tab title click

--- projects/static/projects/main.js
+++ projects/static/projects/main.js
@@ -1,38 +1,43 @@
-function OnTab() {
-    this.on_tab = None;
+class Tab {
+  constructor() {
+    this.enabled_tab = null;
+  }
+  set(e) {
+    return e.currentTarget.id;
+  }
 }
 
-OnTab.prototype.set = function(e) {
-    return e.currentTarget.id;
-};
-
-var on_tab = new OnTab();
+var tab = new Tab();
 
 function on(e) {
-  var element = overlayIterator(e);
+  var element = iterator(e, "background-overlay");
   element.style.display = "block";
   document.body.style.overflow = "hidden";
 }
 
 function off(e) {
   if (e.target !== e.currentTarget) return;
-  var element = overlayIterator(e);
+  var element = iterator(e, "background-overlay");
   element.style.display = "none";
   document.body.style.overflow = "auto";
 }
 
-function overlayIterator(e) {
-  var overlays = document.getElementsByClassName("background-overlay");
-  for (var i = 0; i < overlays.length; i++) {
-    if (overlays[i].id === e.currentTarget.id) {
-      return overlays[i];
+function iterator(e, className) {
+  var elements = document.getElementsByClassName(className);
+  for (var i = 0; i < elements.length; i++) {
+    if (elements[i].id === e.currentTarget.id) {
+      return elements[i];
     }
   }
 }
 
-function tab(e) {
-  on_tab.style.display = "none";
-  OnTab.set(e);
-  var element = document.getElementByID(e.currentTarget.id);
-  element.style.display = block;
-
+function onTab(e) {
+  if (tab.enabled_tab != null) {
+    tab.enabled_tab.style.display = "none";
+  }
+  tab.set(e);
+  var element = iterator(e, "tab-text");
+  console.log(element);
+  console.log(e.currentTarget.id);
+  element.style.display = "block";
+}
