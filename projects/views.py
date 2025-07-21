00966cd88fc8ec19ba7b87e9d3ed07bb866d5395
6MB11 <45149917+6MB11@users.noreply.github.com>
2025-07-21 17:48:43 -0400

    Fixing project link, removing image from overlay

--- projects/views.py
+++ projects/views.py
@@ -4,7 +4,7 @@ from .models import Project, ExternalSite
 # Create your views here.
 
 
-def home(request, project_title):
+def home(request, project_title=None):
     projects = Project.objects.all()
     external_sites = ExternalSite.objects.all()
     context = {'project_title': project_title, 'projects': projects, 'external_sites': external_sites}
