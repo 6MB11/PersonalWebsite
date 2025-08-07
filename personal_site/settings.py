56a3f4bcd4ff6cbb1c446311301ce6b1d4952468
6MB11 <45149917+6MB11@users.noreply.github.com>
2025-08-07 19:29:38 -0400

    Fixing static settings

--- personal_site/settings.py
+++ personal_site/settings.py
@@ -122,9 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'staticfiles/'
+# URL where files will be served from (e.g. static.markobuha.ca)
+STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 
+# Directory where static files are located
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
@@ -200,8 +202,8 @@ CKEDITOR_CONFIGS = {
     }
 }
 
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# Directory where static and media files are collected
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
