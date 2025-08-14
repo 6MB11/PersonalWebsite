d554800ff15d22a8fc8a4286492fdebea823cf82
6MB11 <45149917+6MB11@users.noreply.github.com>
2025-08-13 22:43:41 -0400

    Update settings.py

--- personal_site/settings.py
+++ personal_site/settings.py
@@ -14,7 +14,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from django.core.management.utils import get_random_secret_key
 from csp.constants import NONCE, SELF
-import dj_database_url  # <-- Updated!
 import os
 import environ  # <-- Updated!
 
@@ -101,9 +100,9 @@ WSGI_APPLICATION = 'personal_site.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-  'default': dj_database_url.config(conn_max_age=600, ssl_require=False)
+    # read os.environ['DATABASE_URL']
+    'default': env.db()  # <-- Updated!
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
